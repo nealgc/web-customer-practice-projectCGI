bce34467c40063ced09ef1e03da497bc9a178da2
nealgc <grhmneal@yahoo.co.uk>
2020-12-04 13:38:24 +0000

    Merge log4 java

--- web-customer-practice-projectCGI/src/main/java/com/grahamjava/springmvc/controller/LoginController.java
+++ web-customer-practice-projectCGI/src/main/java/com/grahamjava/springmvc/controller/LoginController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
 
 @Controller
 public class LoginController {
+	
+	 private Logger logger = Logger.getLogger(LoginController.class);
 
 	@RequestMapping("/login")
 	public ModelAndView showUserLoginPage() {
-		
-		System.out.println("in password controller");
- 
+
+	    logger.info("showUserLoginPage() ->  processing loginPage!");
+
 		ModelAndView mv = new ModelAndView("loginPage");
 		return mv;
 	}
@@ -20,9 +23,10 @@ public class LoginController {
 	@PostMapping("/loginCheck")
 	public ModelAndView checkLogin() {
 		
-		System.out.println("in checkLogin()");
+		
+		logger.info("checkLogin() ->  processing list-customer after login!");
  
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("list-customers");
 		return mv;
 	}
 }
